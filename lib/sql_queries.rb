4026baa818edccc99830b33bf1e947dac58a9d03
German Cruz <cruzgerman216@yahoo.com>
2020-04-21 19:51:23 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,7 +32,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   "SELECT users.name, sum(pledges.amount) AS get_amount from users
   INNER JOIN pledges
   ON users.id = pledges.user_id
-  GROUP BY get_AMOUNT"
+  ORDER BY get_AMOUNT"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
